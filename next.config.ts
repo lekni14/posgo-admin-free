34364e0c6eb131e4da3c8d01684e7dc60fcb2645
lekni14 <nipon.ch.54@ubu.ac.th>
2025-12-09 13:13:12 +0700

    add theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/signin",
+  //       permanent: false, // temporary redirect
+  //     },
+  //   ];
+  // },
+  webpack(config: { ignoreWarnings: ({ module: RegExp; message?: undefined; } | { message: RegExp; module?: undefined; })[]; }) {
+    config.ignoreWarnings = [
+      // Ignore warnings from your customStyle.scss
+      {
+        module: /customStyle\.scss/,
+      },
+      // Ignore Webpack cache serialization warnings
+      {
+        message: /No serializer registered for Warning/,
+      },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
